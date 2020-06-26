3eb54951f8a4944d0a2f8bc651d27cf571eb4f8a
etienne <xdze2.me@gmail.com>
2020-06-26 16:23:57 +0200

    clean graph code

--- run_corr.py
+++ run_corr.py
@@ -37,18 +37,15 @@ samples = os.listdir(input_data_dir)
 print('Available samples')
 print('=================')
 ft.print_numbered_list(samples)
-print(' ')
 
 # Select a sample:
-sample_name = 'gris_e_zoom8'
-
-
+sample_name = samples[2]
+print(sample_name)
 
 # +
 # ==================
 #  Load image cube
 # ==================
-print(' ')
 print('Loading image cube...', end='\r')
 
 # List, select and sort images
@@ -67,7 +64,7 @@ print('create outpur directory:', sample_output_path)
 
 plt.figure(); plt.title(f'cube std - {sample_name}');
 plt.imshow(np.std(cube, axis=2));
-plt.savefig(os.path.join(sample_output_path, '01_cube_std.png'));
+plt.savefig(os.path.join(sample_output_path, '01_cube_std.svg'));
 
 # +
 # ==================
@@ -75,10 +72,13 @@ plt.savefig(os.path.join(sample_output_path, '01_cube_std.png'));
 # ==================
 
 window_half_size = 120
-upsample_factor = 20
 
-grid_spacing = 280
-grid_margin = 350
+grid_spacing = 200 #//3
+grid_margin = 350 #//3
+
+
+upsample_factor = 100
+
 
 reference_image = 0
 
@@ -87,38 +87,48 @@ grid = build_grid(cube.shape, margin=grid_margin, spacing=grid_spacing)
 points = np.vstack( [grid[0].flatten(), grid[1].flatten()] ).T
 
 # Graph the grid
+show_pts_number = False
+show_window = False
+
 plt.figure();
 plt.imshow(cube[:, :, reference_image]);
-plt.title(f'Grid - image #{reference_image:02d}');
+plt.title(f'Grille - D={grid_spacing}px - {points.shape[0]} points');
 plt.plot(*grid, 'o', color='white', markersize=3);
 
-for k, (x, y) in enumerate(points):
-    if len(points) > 10 and k % 5 != 0:
-        continue
-    text_offset = 10.0
-    plt.text(x+text_offset, y+text_offset,
-             str(k), fontsize=8, color='white')
+if show_pts_number:
+    for k, (x, y) in enumerate(points):
+        if len(points) > 10 and k % 5 != 0:
+            continue
+        text_offset = 10.0
+        plt.text(x+text_offset, y+text_offset,
+                 str(k), fontsize=8, color='white')
     
-# graph one of the ROI
-box = np.array([[-1, 1, 1, -1, -1], [-1, -1, 1, 1, -1]])*(window_half_size + 1)
-middle_point = tuple(np.array(grid[0].shape) // 2 - 1)
-plt.plot(box[0]+grid[0][middle_point], box[1]+grid[1][middle_point],
-         color='white', linewidth=1)
+if show_window:
+    # graph one of the ROI
+    box = np.array([[-1, 1, 1, -1, -1], [-1, -1, 1, 1, -1]])*(window_half_size + 1)
+    middle_point = tuple(np.array(grid[0].shape) // 2 - 1)
+    plt.plot(box[0]+grid[0][middle_point], box[1]+grid[1][middle_point],
+             color='white', linewidth=1)
 
-plt.savefig(os.path.join(sample_output_path, '02_grid.png'));
+plt.savefig(os.path.join(sample_output_path, '02_grid.svg'));
 
 # +
 # ============================================
 #  Compute image to image displacement field
 # ============================================
-print(' ')
 print('Compute image to image displacement field:')
 # shape: (Nbr points, 2, nbr_frames)
+
+offsets = get_displacement_from_previous(cube, cube.shape[0]//2, cube.shape[1]//2, 
+                                          min(cube.shape[0]//2, cube.shape[1]//2)//2,
+                                upsample_factor=1,
+                                verbose=False)
+
 displacements = np.zeros((*points.shape, cube.shape[2]))
 for k, (x, y) in enumerate(points):
     print(f'{k: 4d}/{len(points)}', end='\r')
     disp_from_prev = get_displacement_from_previous(cube, x, y, 
-                                          window_half_size, upsample_factor,
+                                          window_half_size, upsample_factor, offsets=offsets,
                                           verbose=False)
     displacements[k, :, :] = disp_from_prev.T
         
@@ -127,99 +137,73 @@ print('done', ' '*30)
 
 # -
 
-def plot_vector_field(displacements, view_factor=None):
-    amplitudes = np.sqrt(np.sum( displacements**2, axis=1 )) # disp. amplitude
+def plot_vector_field(points, displacements,
+                      view_factor=None, color='white'):
+    amplitudes = np.sqrt(np.nansum( displacements**2, axis=1 )) # disp. amplitude
 
-    plt.imshow(cube[:, :, image_id]);
-    plt.quiver(*points.T, *displacements.T,
-               angles='xy', color='white',
+    mask = ~np.any( np.isnan(displacements), axis=1, keepdims=False )
+    
+    plt.quiver(*points[mask, :].T, *displacements[mask, :].T,
+               angles='xy', color=color,
                scale_units='xy',
                scale=1/view_factor if view_factor else None,
                minlength=1e-4);
     #plt.title(f'champ de déplacement - image {reference_image}->{image_id} \n d_max={np.nanmax(amplitudes):.2f}px');
-    plt.text(20.1, 50.1,
-             f'd_max={np.nanmax(amplitudes):.2f}px', fontsize=8, color='white')
-
-
-# +
-# ===============
-#  Bilinear Fit
-# ===============
-
-def bilinear_fit(points, displacements):
-    # Least Square
-    u, v = displacements.T
-    mask = ~np.isnan(u) & ~np.isnan(v) 
-    u, v = u[mask], v[mask]
-
-    x, y = points[mask, :].T
-
-    ones = np.ones_like(x)
-    M = np.vstack([x, y, ones]).T
-
-    p_ux, residual_x, rank, s = np.linalg.lstsq(M, u, rcond=None)
-    p_uy, residual_y, rank, s = np.linalg.lstsq(M, v, rcond=None)
-
-    p = np.vstack([p_ux, p_uy])
-    # np.linalg.inv(np.matmul(M.T, M))
-
-    # unbiased estimator variance (see p47 T. Hastie)
-    sigma_hat_x = np.sqrt(residual_x/(M.shape[0]-M.shape[1]-1))
-    sigma_hat_y = np.sqrt(residual_y/(M.shape[0]-M.shape[1]-1))
-
-    # Residus 
-    u_linear = np.matmul( M, p_ux )
-    v_linear = np.matmul( M, p_uy )
-
-    residus_x = u - u_linear
-    residus_y = v - v_linear
-
-    residus_xy = np.vstack([residus_x, residus_y])
-    
-    return p, residus_xy#(sigma_hat_x, sigma_hat_y)
-# -
-
+    plt.text(10., 10.,
+             f'max(|u|)={np.nanmax(amplitudes):.2f}px  mean(|u|)={np.nanmean(amplitudes):.2f}px',
+             fontsize=12, color=color,
+             verticalalignment='top')
 
 
 # +
 # Quiver Graph
-output_dir = 'frame_to_frame_disp'
+output_dir = f'frame_to_frame_window{window_half_size}px'
+image_ext = "svg"
+
 save_path = os.path.join(sample_output_path, output_dir)
 ft.create_dir(save_path)
-
-for image_id in range(displacements.shape[-1]):
+for image_id in range(1, displacements.shape[-1]):
+    
+    # 1. Champ de déplacement
     disp_k = displacements[:, :, image_id]
-    # Champ de déplacement
+    
     plt.figure();
-    disp_k_ss_translation = disp_k - np.nanmean(disp_k, axis=0)
-    plot_vector_field(disp_k_ss_translation, view_factor=None)
-    plt.title(f'champ de déplacement - image {image_id-1}->{image_id}');
-    filename = f'disp_field_{image_id:04d}.png'
+    plt.imshow(cube[:, :, image_id]);
+    plot_vector_field(points, disp_k, view_factor=None)
+    plt.title(f'champ de déplacement - images {image_id-1}→{image_id} - fenêtre:{window_half_size*2+1}px');
+    filename = f'disp_field_{image_id:04d}.{image_ext}'
     plt.savefig(os.path.join(save_path, filename));
     print(f'figure saved: {filename}')
     plt.close()
     
-    # fit
-    p, residus = bilinear_fit(points, disp_k)
-    amplitudes = np.sqrt(np.nansum( residus**2, axis=1 )) # disp. amplitude
-    view_factor = None
+    # 2. Sans la translation
+    disp_k_ss_translation = disp_k - np.nanmean(disp_k, axis=0)
+    
     plt.figure();
-    plt.title(f'résidus après fit linéaire - image {image_id-1}->{image_id}');
     plt.imshow(cube[:, :, image_id]);
-    plt.quiver(*points.T, *residus,
-               angles='xy', color='red',
-               scale_units='xy',
-               scale=1/view_factor if view_factor else None,
-               minlength=1e-4);
-    plt.text(20.1, 50.1,
-             f'max={np.nanmax(amplitudes):.2f}px',
-             fontsize=8, color='red')
-    filename = f'residuals_{image_id:04d}.png'  
+    plot_vector_field(points, disp_k_ss_translation, view_factor=None)
+    plt.title(f'sans la translation - images {image_id-1}→{image_id} - fenêtre:{window_half_size*2+1}px');
+    filename = f'without_translation_{image_id:04d}.{image_ext}'
     plt.savefig(os.path.join(save_path, filename));
+    print(f'figure saved: {filename}')
+    plt.close()
+    
+    # 3. fit bilineaire
+    p, residus = bilinear_fit(points, disp_k)
+    #amplitudes = np.sqrt(np.nansum( residus**2, axis=0 )) # disp. amplitude
+    view_factor = None
+    
+    plt.figure();
+    plt.title(f'résidus après fit linéaire - images {image_id-1}→{image_id} - fenêtre:{window_half_size*2+1}px');
+    plt.imshow(cube[:, :, image_id]);
+    plot_vector_field(points, residus, view_factor=None, color='red')
+    filename = f'residuals_{image_id:04d}.{image_ext}'  
+    plt.savefig(os.path.join(save_path, filename));
+    print(f'figure saved: {filename}')
     plt.close()
 # -
 
-disp_k_ss_translation = disp_k - np.mean(disp_k, axis=0)
+p
 
 # +
 print('')
