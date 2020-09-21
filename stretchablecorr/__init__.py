75c6f1f2c856ecf3865ed21b35a96577a1cc56b2
etienne <xdze2.me@gmail.com>
2020-09-21 16:57:33 +0200

    add plot_cross_corr

--- stretchablecorr/__init__.py
+++ stretchablecorr/__init__.py
@@ -4,4 +4,4 @@ from .stretchablecorr import *
 from .filetools import *
 from .graphplot import *
 from .postprocess import *
-from .opti_registration import output_cross_correlation
+from .opti_registration import *
